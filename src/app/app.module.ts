b9cdf987dd4de02a6f8fa760abc08a831fe58bbe
Olga Kulikova <masyakot@gmail.com>
2017-02-25 21:26:07 +0200

    app component as shel for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,30 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }      from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 import { AppComponent }  from './app.component';
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports: [ 
+  	BrowserModule, 
+  	FormsModule,
+  	RouterModule.forRoot([
+	  {
+	    path: 'heroes',
+	    component: HeroesComponent
+	  }
+	])
+  ],
+  declarations: [ 
+  	AppComponent, 
+  	HeroDetailComponent, 
+  	HeroesComponent 
+  ],
+  providers: [
+    HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
