0724b317bc7c7d15243b579680d75f82879c5ed4
Olga Kulikova <masyakot@gmail.com>
2017-02-25 21:37:50 +0200

    added router

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -16,7 +16,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<my-heroes></my-heroes>\n\t"
+            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<a routerLink=\"/heroes\">Heroes</a>\n   \t\t<router-outlet></router-outlet>\n\t"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
